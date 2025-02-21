97b4598221f464257cb5ca53578abf9de594135a
jaac-dev <j4ckk3@gmail.com>
2025-02-21 15:33:16 -0600

    fixed build system

---
+++ cmake/gsdk.cmake
@@ -0,0 +1,6 @@
+set(GSDK "${CMAKE_CURRENT_LIST_DIR}/../sdk")
+
+include("${CMAKE_CURRENT_LIST_DIR}/configs/base.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/configs/lib.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/configs/dll.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/platform.cmake")
